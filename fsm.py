bc307e4d05899be728616c5aa2c62cd67ffaa348
Kassra-sinaei <kasra.sinaei@gmail.com>
2025-05-10 14:44:59 -0400

    correction to tf frames, approach controller

--- fsm.py
+++ fsm.py
@@ -2,7 +2,7 @@
 import pinocchio as pin
 from pinocchio.robot_wrapper import RobotWrapper
 import numpy as np
-import time
+import time, threading
 from math import atan2
 
 import matplotlib.pylab as plt
@@ -26,6 +26,8 @@ class FSM(Node):
     def __init__(self, type, side, width):
         # ROS2 Node Setup
         super().__init__('fsm')
+        self.rate = self.create_rate(20)
+
         self.joint_state_sub = self.create_subscription(JointState, '/isaac_sim/joint_states', self.jointStateCallback, 2)
         self.base_pose_sub = self.create_subscription(TFMessage, '/tf', self.basePoseCallback, 2)
         self.handle_sub = self.create_subscription(TFMessage, '/isaac_sim/door_handle', self.handleCallback, 2)
@@ -62,7 +64,7 @@ class FSM(Node):
         self.r_gripper = self.model.getFrameId('r_gripper_frame')
         self.l_gripper = self.model.getFrameId('l_gripper_frame')
         self.base = self.model.getFrameId('base_link_underpan')
-        joint_names = [self.robot.model.names[i] for i in range(self.robot.model.njoints)]
+        # joint_names = [self.robot.model.names[i] for i in range(self.robot.model.njoints)]
         # print("Joint names:", joint_names)
 
         # Setup Pink
@@ -73,12 +75,13 @@ class FSM(Node):
             'l_gripper':FrameTask('l_gripper_frame', position_cost=1.0, orientation_cost=1.0)
         }
         self.joint_names = ['platform_joint',
-                            'r_joint1', 'r_joint2', 'r_joint3', 'r_joint4', 'r_joint5', 'r_joint6'
+                            'r_joint1', 'r_joint2', 'r_joint3', 'r_joint4', 'r_joint5', 'r_joint6',
                             'l_joint1', 'l_joint2', 'l_joint3', 'l_joint4', 'l_joint5', 'l_joint6']
         # Setup Crocoddyl
         self.base_model = crocoddyl.ActionModelUnicycle()
         self.base_model.dt = 0.1
-        self.base_model.costWeights = np.matrix([5, 20]).T
+        self.base_model.costWeights = np.matrix([5, 4]).T
+        self.base_model.stateWeights = np.matrix([1, 1, 5]).T
         self.data  = self.base_model.createData()
 
         # Door Parameters
@@ -86,12 +89,12 @@ class FSM(Node):
         self.handle_side = side   # left or right
         self.door_width = width   # width of the door
         if self.handle_side == "left":
-            self.handle_offset = np.array([1.2, 0.15])
+            self.handle_offset = np.array([1.15, 0.20])
         else:
-            self.handle_offset = np.array([1.2, -0.15])
+            self.handle_offset = np.array([1.15, -0.20])
 
     def unicycleController(self, start, goal, T):
-        e = goal - start
+        e = (start - goal)
         e.reshape(3, 1)
         T = int(T / self.base_model.dt)
         problem = crocoddyl.ShootingProblem(e, [ self.base_model ] * T, self.base_model)
@@ -124,7 +127,7 @@ class FSM(Node):
                                    msg.transforms[0].transform.translation.y,
                                    psi])
         print(psi)
-        if self.state == "initial" and np.linalg.norm(self.base_pose[0:2] - (self.handle_pose[0:2] - self.handle_offset)) < 0.1 and abs(psi - np.pi) < 0.1:
+        if self.state == "initial" and np.linalg.norm(self.base_pose[0:2] - (self.handle_pose[0:2] - self.handle_offset)) < 0.1 and abs(psi) < 0.1:
             self.state = "approach"
         # rclpy.logging.get_logger('fsm').info("Base Pose updated ...")
         pass
@@ -160,8 +163,9 @@ class FSM(Node):
     def approach(self):
         goal = np.zeros(3)
         goal[0:2] = self.handle_pose[0:2] - self.handle_offset
-        goal[2] = atan2(2.0 * (self.handle_pose[6] * self.handle_pose[5] + self.handle_pose[3] * self.handle_pose[4]), 
-                        1.0 - 2.0 * (self.handle_pose[4] * self.handle_pose[4] + self.handle_pose[5] * self.handle_pose[5]))
+        # goal[2] = atan2(2.0 * (self.handle_pose[6] * self.handle_pose[5] + self.handle_pose[3] * self.handle_pose[4]), 
+        #                 1.0 - 2.0 * (self.handle_pose[4] * self.handle_pose[4] + self.handle_pose[5] * self.handle_pose[5]))
+        goal[2] = 0
         if self.state == "initial":
             try:
                 res = self.unicycleController(self.base_pose, goal, 10)
@@ -175,32 +179,36 @@ class FSM(Node):
 
 
     def grasp(self):
-        if self.state == "approach":
-            if self.door_type == "push":
-                local_handle = np.array([self.handle_pose[0] - self.base_pose[0], 
-                                         self.handle_pose[1] - self.base_pose[1], 
-                                         self.handle_pose[2]])
-                if self.handle_side == "left":
-                    r_goal_p = local_handle
-                    r_goal_q = pin.Quaternion(self.handle_pose[6], self.handle_pose[3], self.handle_pose[4], self.handle_pose[5]).normalized().matrix()
-                    l_goal_p = np.array([0.2, 0.2, 1.5])
-                    l_goal_q = pin.Quaternion(1, 0, 0, 0).normalized().matrix()
-                else:
-                    l_goal_p = local_handle
-                    l_goal_q = pin.Quaternion(self.handle_pose[6], self.handle_pose[3], self.handle_pose[4], self.handle_pose[5]).normalized().matrix()
-                    r_goal_p = np.array([0.2, -0.2, 1.5])
-                    r_goal_q = pin.Quaternion(1, 0, 0, 0).normalized().matrix()
-                q = self.pinkIK(r_goal_p, r_goal_q, l_goal_p, l_goal_q)
-                msg = JointState()
-                msg.name = self.joint_names
-                msg.position = [q[0], 
-                                q[9], q[10], q[11], q[12], q[13], q[14],
-                                q[3], q[4], q[5], q[6], q[7], q[8]]
-                self.joint_state_pub.publish(msg)
-                print("publishing joint state ...")
-                                
+        msg = Twist()
+        msg.linear.x = 0.0
+        msg.angular.z = 0.0
+        self.base_pub.publish(msg)
+        if self.door_type == "push":
+            local_handle = np.array([self.handle_pose[0] - self.base_pose[0], 
+                                        self.handle_pose[1] - self.base_pose[1], 
+                                        self.handle_pose[2] - 0.243])
+            if self.handle_side == "left":
+                r_goal_p = local_handle
+                r_goal_q = pin.Quaternion(self.handle_pose[6], self.handle_pose[3], self.handle_pose[4], self.handle_pose[5]).normalized().matrix()
+                l_goal_p = np.array([0.4, 0.2, 1.5])
+                l_goal_q = pin.Quaternion(1, 0, 0, 0).normalized().matrix()
+            else:
+                l_goal_p = local_handle
+                l_goal_q = pin.Quaternion(self.handle_pose[6], self.handle_pose[3], self.handle_pose[4], self.handle_pose[5]).normalized().matrix()
+                r_goal_p = np.array([0.2, -0.2, 1.5])
+                r_goal_q = pin.Quaternion(1, 0, 0, 0).normalized().matrix()
+            q = self.pinkIK(r_goal_p, r_goal_q, l_goal_p, l_goal_q)
+            msg = JointState()
+            msg.name = self.joint_names
+            msg.position = [q[0], 
+                            q[9], q[10], q[11], q[12], q[13], q[14],
+                            q[3], q[4], q[5], q[6], q[7], q[8]]
+            self.joint_state_pub.publish(msg)
+            print("publishing joint state ...")
         else:
-            print("Not in approach state, cannot grasp ...")
+            # TODO: implement pull type door grasping
+            pass
+                                
 
 def test(fsm):
     r_goal_p = np.array([-0.4, -0.4, 1.0])
@@ -233,19 +241,24 @@ if __name__ == "__main__":
     rclpy.init()
     fsm = FSM('push', 'left', 0.9)
     # test(fsm)
-    time.sleep(2)
-    rclpy.spin_once(fsm)
-    rclpy.spin_once(fsm)
-    while rclpy.ok():
-        rclpy.spin_once(fsm)
-        time.sleep(0.05)
-        if fsm.state == "initial":
-            fsm.approach()
-        # elif fsm.state == "approach":
-        #     fsm.grasp()
-        else:
-            print("Exiting Program  ...")
-            fsm.destroy_node()
-            rclpy.shutdown()
-            break     
+
+    thread = threading.Thread(target=rclpy.spin, args=(fsm, ), daemon=True)
+    thread.start()
+
+    try:
+        while rclpy.ok():
+            if fsm.state == "initial":
+                fsm.approach()
+            elif fsm.state == "approach":
+                fsm.grasp()
+            else:
+                break
+            fsm.rate.sleep()
+
+    except KeyboardInterrupt:
+        pass
     
+    rclpy.logging.get_logger('fsm').info("Keyboard Interrupt, shutting down ...")
+    fsm.destroy_node()
+    rclpy.shutdown()
+    thread.join()
